cfe4fe2a74b1273f23748a840db62c575686970b
mishrasunny174 <mishrasunny174@gmail.com>
2018-11-19 15:37:27 +0530

    completed practical 8

--- Practical8/app/src/main/java/io/github/mishrasunny174/practical8/MainActivity.java
+++ Practical8/app/src/main/java/io/github/mishrasunny174/practical8/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
     private Button loginButton;
     private Button createAccountButton;
     private Button listUsersButton;
+    private Button removeUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +20,19 @@ public class MainActivity extends Activity {
         loginButton = (Button)findViewById(R.id.login_button);
         createAccountButton = (Button)findViewById(R.id.create_account);
         listUsersButton = (Button)findViewById(R.id.list_users);
+        removeUser = (Button)findViewById(R.id.delete_user_button);
         createAccountButton.setOnClickListener((view)->{
             Intent intent = new Intent(this,CreateAccount.class);
             startActivity(intent);
         });
         listUsersButton.setOnClickListener((view)->{
             startActivity(new Intent(this,ListUsers.class));
+        });
+        loginButton.setOnClickListener((view)->{
+            startActivity(new Intent(this,login.class));
+        });
+        removeUser.setOnClickListener((view)->{
+            startActivity(new Intent(this,RemoveUser.class));
         });
     }
 }
